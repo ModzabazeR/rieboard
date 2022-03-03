1fad26f725eb47b62873fcd0475a88a8c8086cee
ModzabazeR <moddaeng.pps@gmail.com>
2022-03-03 12:10:10 +0700

    update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,24 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import React, { useEffect, useRef } from "react";
 
-export default function Custom404() {
+const Custom404 = () => {
+  const EndRef = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    EndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [])
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center h-screen" ref={EndRef}>
         <Image
           src="https://walfiegif.files.wordpress.com/2021/01/out-transparent-7.gif"
           alt="tap tap"
@@ -20,3 +30,5 @@ export default function Custom404() {
     </motion.div>
   );
 }
+
+export default Custom404;
